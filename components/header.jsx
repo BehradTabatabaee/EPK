733743621cdd2642759c6c0c3e68b0f8d2b411c1
Ali Mahallati <mahallati81@gmail.com>
2023-09-09 19:59:25 +0330

    navbar created (not responsive)

---
+++ components/header.jsx
@@ -0,0 +1,10 @@
+import NavBar from "./navbar";
+export default function Header() {
+	return (
+		<>
+			<div className="bg-[#3d4e58] w-screen h-screen md:h-[90vh] lg:h-[85vh] xl:h-[80vh] 2xl:h-[75vh]">
+				<NavBar />
+			</div>
+		</>
+	);
+}
