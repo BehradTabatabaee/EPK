a5515d15d3566012c23de528505539893c1c2e87
Amin Ehterami <A.Ehterami@proton.me>
2023-09-13 02:49:13 +0330

    seeder and someother things

---
+++ app/dashboard/page.js
@@ -0,0 +1,8 @@
+export default function Dashboard(){
+    return (<div className="">
+        <div className="w-full h-14 bg-gray-600 fixed"><div>داشبورد</div>
+        </div>
+        
+    </div>
+    )
+}
