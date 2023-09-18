7ca60906915cbf08bfb0c448bec4a60dff3bea4c
Amin Ehterami <A.Ehterami@proton.me>
2023-09-18 17:01:53 +0330

    deleted someshit to test the merging

--- app/dashboard/page.js
+++
@@ -1,8 +0,0 @@
-export default function Dashboard(){
-    return (<div className="">
-        <div className="w-full h-14 bg-gray-600 fixed"><div>داشبورد</div>
-        </div>
-        
-    </div>
-    )
-}
