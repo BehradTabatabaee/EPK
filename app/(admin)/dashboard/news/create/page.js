6796a5134213b67be236927d7c45d44521e5e647
Ali Mahallati <mahallati81@gmail.com>
2023-10-06 22:06:33 +0330

    ckeditor configured & news page completed

--- app/(admin)/dashboard/news/create/page.js
+++ app/(admin)/dashboard/news/create/page.js
@@ -2,54 +2,89 @@
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { Button } from "@/components/ui/button";
-import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
-} from "@/components/ui/card";
-import convertStringToHTML from "@/utils/stringToHtml";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import UploadForm from "@/components/adminDashboard/UploadForm";
+import { useState } from "react";
+const editorConfiguration = {
+	toolbar: {
+		items: ["undo", "redo"],
+	},
+	language: {
+		ui: "en",
+		content: "ar",
+	},
+};
 export default function CreateBlog() {
-	let finalData;
+	const [data, setData] = useState({
+		title: "",
+		text: "",
+		image: "",
+	});
 	return (
 		<>
-			<Card className="m-auto mt-52">
+			<Card className="m-auto overflow-y-scroll">
 				<CardHeader>
 					<CardTitle className="mb-3 font-sans">اضافه کردن مقاله</CardTitle>
-					<CardDescription className="font-sans">
-						لطفا مقاله ی خود را بنویسید .
-					</CardDescription>
 				</CardHeader>
-				<CardContent className="flex flex-col gap-3">
-					<div className="App w-[50vw] font-vazir">
-						<CKEditor
-							editor={ClassicEditor}
-							onReady={(editor) => {
-								console.log(
-									"CKEditor5 React Component is ready to use!",
-									editor
-								);
-							}}
-							onChange={(event, editor) => {
-								const data = editor.getData();
-								finalData = data;
-								console.log({ event, editor, data });
-							}}
-						/>
+				<CardContent className="flex flex-col gap-2">
+					<div className="App w-[50vw] font-vazir flex flex-col gap-5">
+						<div className="flex flex-col gap-3">
+							<label htmlFor="title" className="font-sans w-full">
+								عنوان :
+							</label>
+							<Input
+								autoComplete="off"
+								type="text"
+								name="title"
+								defaultValue={data.title}
+								onChange={(e) =>
+									setData({
+										...data,
+										title: e.target.value,
+									})
+								}
+								className="font-vazir border-2"
+							/>
+						</div>
+						<div className="flex flex-col gap-3">
+							<label htmlFor="title" className="font-sans w-full">
+								متن :
+							</label>
+							<CKEditor
+								editor={ClassicEditor}
+								config={editorConfiguration}
+								onReady={(editor) => {
+									console.log(
+										"CKEditor5 React Component is ready to use!",
+										editor
+									);
+								}}
+								onChange={(event, editor) => {
+									const textInput = editor.getData();
+									setData({
+										...data,
+										text: textInput,
+									});
+								}}
+							/>
+						</div>
+						<div className="flex flex-col gap-3">
+							<label htmlFor="title" className="font-sans w-full">
+								عکس :
+							</label>
+							<UploadForm Data={data} SetData={setData} />
+						</div>
 					</div>
 					<Button
 						className="text-base w-28 font-sans"
 						onClick={() => {
-							console.log(convertStringToHTML(finalData));
+							// ersal data be database
 						}}
 					>
 						اضافه کردن
 					</Button>
 				</CardContent>
-
-				<CardFooter></CardFooter>
 			</Card>
 		</>
 	);
