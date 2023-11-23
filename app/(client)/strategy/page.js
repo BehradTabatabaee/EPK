c37af9f1be7f1ee9d2774f6824fd53c21ae1de09
Ali Mahallati <mahallati81@gmail.com>
2023-11-23 17:40:00 +0330

    created strategy page and changed a option footer

---
+++ app/(client)/strategy/page.js
@@ -0,0 +1,59 @@
+export default function Startegy() {
+	return (
+		<>
+			<div className="flex flex-col lg:p-10 p-4 justify-center items-center">
+				<p className="font-sans lg:text-3xl text-center text-2xl lg:mt-4 mr-2 sm:mt-10 mt-16">
+					چشم انداز، اهداف و استراتژی
+				</p>
+				<img
+					src="footer-red.png"
+					alt="divider"
+					className="lg:w-[30rem] md:w-[24rem] sm:w-[19rem] w-[14rem] hidden lg:block mt-5"
+				/>
+			</div>
+			<div className="flex flex-wrap 2xl:justify-between 2xl:items-start justify-center items-center mt-6">
+				<div className="flex flex-wrap 2xl:w-1/2 lg:w-full lg:p-12 !pt-0 sm:w-[35rem] w-[20rem]">
+					<div className="flex flex-col gap-6 text-center">
+						<div className="flex flex-col gap-3">
+							<h1 className="font-sans font-bold text-2xl sm:mr-3">
+								چشم انداز ما{" "}
+							</h1>
+							<p className="font-vazir text-lg mt-2 lg:mr-5">
+								سازمانی رقابتی در کلاس جهانی با سود آوری پایدار و سرآمد در ایمنی
+							</p>
+						</div>
+						<div className="flex flex-col gap-3">
+							<p className="font-vazir sm:text-xl text-lg lg:p-4 p-2 xl:p-16 !py-6 leading-loose 2xl:leading-relaxed">
+								هدف شرکت جلب رضایت مشتریان با ارائه خدمات به هنگام ، کیفی و
+								استاندارد می باشد و در همین راستا ، مجموعه ی مدیریت و کارکنان
+								شرکت می کوشیم به منظور ایفای مسئولیت های اجتماعی خود ، همواره
+								اصول اخلاقی و وجدان کاری ، رعایت حقوق مشتریان ، صداقت ، شفافیت و
+								امانت داری را سر لوحه ی خویش قرار دهیم و در اقدام و عمل و تنظیم
+								روابط حرفه ای ، پایبندی خویش را به این ارزش ها و اهداف جامعه ی
+								عمل پوشانده و در این امر موفقیت لازم را کسب نماییم
+							</p>
+						</div>
+						<div className="flex flex-col gap-3">
+							<h1 className="font-sans font-bold text-2xl sm:mr-3">اهداف </h1>
+							<p className="font-vazir text-lg mt-2 sm:mr-5 sm:p-0 p-4 pb-0">
+								1- افزایش تولید از طریق ارتقای بهره وری و بهبود سیستم ها
+							</p>
+							<p className="font-vazir text-lg mt-2 sm:mr-5 sm:p-0 p-4 pb-0">
+								2- اجرای پروژه های توسعه ای در جهت تکمیل زنجیره ارزش
+							</p>
+							<p className="font-vazir text-lg mt-2 sm:mr-5 sm:p-0 p-4 pb-0">
+								3- ارتقای کیفیت و تکمیل سبد محصول و نوآوری متناسب با نیازهای
+								بازار به منظور افزایش سودآوری
+							</p>
+						</div>
+					</div>
+				</div>
+				<img
+					src="/vision-mission-values.jpg"
+					alt="pic"
+					className="2xl:w-[44rem] xl:w-[49rem] lg:w-[43rem] md:w-[40rem] sm:w-[30rem] w-[20rem] rounded-lg 2xl:ml-12 ml-0 mt-16"
+				/>
+			</div>
+		</>
+	);
+}
